e467ef273a4357d8567ceab0518e27c5e3156e54
MartinSimango <shukosimango@yahoo.com>
2018-03-27 17:38:34 +0200

    create user Input functionality with getPos function

--- __main__.py
+++ __main__.py
@@ -1,13 +1,46 @@
-import data 
+#import data 
+import board
+
+from data import *
+
+def getPos(what:str):
+    print(what)
+    row=input("Row: ").upper()
+    try:                        #try for exception incase user enters space
+        row=row[0];
+    except IndexError:
+        print("Please enter a character for the row!")
+        return getPos(what)
+    try:
+        col=int(input("Column: "))
+    except ValueError:
+        print("\nPlease enter a int for a column!")
+        return getPos(what)
+    return (row,col);  
+    
+def getPlayerMove(Game):
+    return;  
+
+def runGame(game)-> None:
+   # printOut(game.board)
+ #  getPlayerMove(game)
+    
+    #game.
+    return
+    
 
 def main(): 
-    player1 = data.Player("Player1", 'x', 12, "MOVING", [])
-    player2 = data.Player("Player2", 'o', 12, "MOVING", [])
-
-
-
-
-
-
-
-
+    
+  #  player_1= Player("Player 1",'x',12,ThePlayerState.MOVING,[])
+   # player_2= Player("Player 2",'o',12,ThePlayerState.MOVING,[])
+  #  startBoard= Board()
+    #print("Hi\n");
+    #input("Enter something");
+    row,col=getPos("Where do you want to move the cow?")
+    print(row)
+    print(col)
+    return 
+                         
+    #Morabaraba= Game(player_1,player_2,startBoard,0);
+    # runGame(Morabarba)
+main()
