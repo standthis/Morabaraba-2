6dfe10d815d5a5999b9508f4cf6a61e48ab07810
standthis <standthis@protonmail.ch>
2018-03-27 13:10:22 +0200

    Mills definition

--- data.py
+++ data.py
@@ -12,38 +12,39 @@ class Cow:
         self.Symbol = symbol 
         self.PossibleMoves = possibleMoves 
 
+A1 = Cow(('A', 1), ' ', [])
+A4 = Cow(('A', 4), ' ', [])
+A7 = Cow(('A', 7), ' ', [])
+
+B2 = Cow(('B', 2), ' ', [])
+B4 = Cow(('B', 4), ' ', [])
+B6 = Cow(('B', 6), ' ', [])
+
+C3 = Cow(('C', 3), ' ', [])
+C4 = Cow(('C', 4), ' ', [])
+C5 = Cow(('C', 5), ' ', [])
+
+D1 = Cow(('D', 1), ' ', [])
+D2 = Cow(('D', 2), ' ', [])
+D3 = Cow(('D', 3), ' ', [])
+
+D5 = Cow(('D', 5), ' ', [])
+D6 = Cow(('D', 6), ' ', [])
+D7 = Cow(('D', 7), ' ', [])
+
+E3 = Cow(('E', 3), ' ', [])
+E4 = Cow(('E', 4), ' ', [])
+E5 = Cow(('E', 5), ' ', [])
+
+F2 = Cow(('F', 2), ' ', [])
+F4 = Cow(('F', 4), ' ', [])
+F6 = Cow(('F', 6), ' ', [])
+
+G1 = Cow(('G', 1), ' ', [])
+G4 = Cow(('G', 4), ' ', [])
+G7 = Cow(('G', 7), ' ', [])
+
 def startBoard():
-    A1 = Cow(('A', 1), ' ', [])
-    A4 = Cow(('A', 4), ' ', [])
-    A7 = Cow(('A', 7), ' ', [])
-
-    B2 = Cow(('B', 2), ' ', [])
-    B4 = Cow(('B', 4), ' ', [])
-    B6 = Cow(('B', 6), ' ', [])
-
-    C3 = Cow(('C', 3), ' ', [])
-    C4 = Cow(('C', 4), ' ', [])
-    C5 = Cow(('C', 5), ' ', [])
-    
-    D1 = Cow(('D', 1), ' ', [])
-    D2 = Cow(('D', 2), ' ', [])
-    D3 = Cow(('D', 3), ' ', [])
-
-    D5 = Cow(('D', 5), ' ', [])
-    D6 = Cow(('D', 6), ' ', [])
-    D7 = Cow(('D', 7), ' ', [])
-
-    E3 = Cow(('E', 3), ' ', [])
-    E4 = Cow(('E', 4), ' ', [])
-    E5 = Cow(('E', 5), ' ', [])
-    
-    F2 = Cow(('F', 2), ' ', [])
-    F4 = Cow(('F', 4), ' ', [])
-    F6 = Cow(('F', 6), ' ', [])
-
-    G1 = Cow(('G', 1), ' ', [])
-    G4 = Cow(('G', 4), ' ', [])
-    G7 = Cow(('G', 7), ' ', [])
 
     A1.PossibleMoves = [A4, B2, D1]
     A4.PossibleMoves = [A1, A7, B4]
@@ -79,3 +80,31 @@ def startBoard():
 
     return [A1, A4, A7, B2, B4, B6, C3, C4, C5, D1, D2, D3, D5, D6, D7, E3, E4, E5, F2, F4, F6, G1, G4, G7]
 
+def allBoardMills():
+
+    AA17 = [A1, A4, A7]
+    BB26 = [B2, B4, B6]
+    CC35 = [C3, C4, C5]
+    DD13 = [D1, D2, D3]
+    DD57 = [D5, D6, D7]
+    EE35 = [E3, E4, E5]
+    FF26 = [F2, F4, F6]
+    GG17 = [G1, G4, G7]
+
+    AG11 = [A1, D1, G1]
+    BF22 = [B2, D2, F2]
+    CE33 = [C3, D3, E3]
+    AC44 = [A4, B4, C4]
+    EG44 = [E4, F4, G4]
+    CE55 = [C5, D5, E5]
+    BF66 = [B6, D6, F6]
+    AG77 = [A7, D7, G7]
+
+    AC13 = [A1, B2, C3]
+    CA57 = [C5, B6, A7]
+    GE13 = [G1, F2, E3]
+    EG57 = [E5, F6, G7]
+
+    allBoardMills = [ AA17, BB26, CC35, DD13, DD57, EE35, FF26, GG17, AG11, BF22, CE33, AC44, EG44, CE55, BF66, AG77, AC13, CA57, GE13, EG57 ]
+
+    return allBoardMills
