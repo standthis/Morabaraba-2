b67ffa031d94c22de66d5cb6678ea1172bb8d333
standthis <standthis@protonmail.ch>
2018-03-28 17:11:21 +0200

    main 2.0 - rework to use developed game functions

--- __main__.py
+++ __main__.py
@@ -1,113 +1,54 @@
-#import data 
-import board
-
-from data import *
-
-
-
-def getPos(what:str):
-    print(what)
-    row=input("Row: ").upper()
-    try:                        #try for exception incase user enters space
-        row=row[0];
-    except IndexError:
-        print("Please enter a character for the row!")
-        return getPos(what)
-    try:
-        col=int(input("Column: "))
-    except ValueError:
-        print("\nPlease enter a int for a column!")
-        return getPos(what)
-    return (row,col);  
-    
-def getPlayerMove(game:Game,player:Player):        
-    if(player.PlayerState==ThePlayerState.MOVING):
-        
-        toRow,toCol=getPos("{0}'s turn\nWhere do you want to place the cow?".format(player.Name));
-        
-        #if(game.isValidPlace((row,col))): //check if the positions is in the available board
-            #make placeholder from  and to pos's
-            fromRow='Z'
-            fromCol=100;
-            # return (fromRow,toCol),(toRow,toCol)
-        else:
-            printf("Can't move cow to ({0},{1}})".format(toRow,toCol))
-            getPlayerMove(game,player)
-                         
-    elif(player.PlayerState==ThePlayerState.PLACING or player.PlayerState==ThePlayerState.FLYING):
-        
-         fromRow,fromCol=getPos("{0}'s turn\nWhere do you want movethe cow from?".format(player.Name))
-        
-       #  if(game.isValidFrom(player,(fromRow,fromCol))): //check if player has postion (fromRow,fromCol)
-              toRow,toCol=getPos("{0}'s turn\nWhere do you want move the cow to?".format(player.Name))
-         #     if(game.isValidTo(player,(toRow,toCol))): //check if player can move to that available board checking their state too
-               # return (fromRow,toCol),(toRow,toCol) 
-              else:
-                 printf("Can't move from ({0},{1})cow to ({2},{3})".format(fromRow,fromCol,toRow,toCol))
-                 #return getPlayerMove(game,player)
-                  
-         else:
-            printf("You have now cow at ({0},{1}}".format(fromRow,fromCol))
-           #return getPlayerMove(game,player)
-
-#def isInMill(pos,playerMills):
-    
-
-#def canKillCowInMill(enemyPlayer,playerMills): #check if every pos in enemyPlayer's position is in playerMills
-    
-def killCow(game:Game,enemyPlayer:Player):
-  
-   playerMills=game.getPlayerMills(enemyPlayer); 
-   row,col=getPos("What cow do you want to kill?")
-   if(game.isValidKillPlace(row,col)): #check to see if(row,col) is in enemy's positons
-       if(isInMill((row,col),playerMills)):
-           if(canKillCowInMill(enemyPlayer,playerMills)):
-                  return (row,col)
-           else:
-                 print("Can't kill cow in mill unless all cows are in mills\n") 
-                 killCow(game)
-       else:
-           return (row,col)
-   else:
-        print("No valid cow was in pos ({0},{1})\n".format(row,col))
-        killCow(game) 
-                          
-def runGame(game:Game)-> None:
-    if(game.turn==0):       #player 1's turn
-        player=game.Player1
-        enemyPlayer=game.Player2
-    else:                   #player 2's turn
-        player=game.Player2
-        enemyPlayer=game.Player1
-        
-    board.printOut(game.Board)
-    fromPos, toPos= getPlayerMove(game,player)
-    #game.updatePlayer((fromRow,fromCol),(toRow,toCol));  //update the board and player positions based on the players state
-                                  #game will know who's turn it is and will return the appropriate player
-    board.printOut(game.Board)
-    #if(game.checkFromMills()):  //check if a board mills was formed by the current play
-       #killRow,killCol=killCow(game,enemyPlayers)
-       #game.killCow((row,col));
-     #if(game.Ended()): //game should check if enemy player cant move and that enemy player has more than 2 cows (IF FLYING)
-     # game.printEndMessage() //prints out the appropriate ending message  
-     # return
-     #else    
-     #    game.changePlayerTurn() 
-     #    return runGame(game)
-     
-  
-    
-
-                               
-    
+from data import * 
+from board import printOut  
 
 def main():
-    player_1= Player("Player 1",'x',12,ThePlayerState.MOVING,[])
-    player_2= Player("Player 2",'o',12,ThePlayerState.MOVING,[])
-    startBoard= Board()
-    game= Game(player_1,player_2,startBoard,0);  
-    runGame(game,game.Player1,game.Player2);                  
-    return 
+    player1 = Player("player1", 'X', 12, ThePlayerState.PLACING, [])
+    player2 = Player("player2", 'O', 12, ThePlayerState.PLACING, [])
+    board = Board()
+    startboard = board.startBoard()
+    allBoardMills = board.allBoardMills()
+    game = Game(player1, player2, startboard, 1)
+    while True:
+        game.nextTurn()
+        print(game.CurrentPlayer.Name)
+        printOut(game.Board)
+        getPlayerMove(game.CurrentPlayer, game.availableBoard(), allBoardMills)
 
-                
+
+def getPos():
+    row = input("What row ")
+    row = row.upper()
+    col = int(input("What col "))
+    return (row, col) 
+
+def getPlayerMove(player, availableBoard, allBoardMills):
+    if player.PlayerState == ThePlayerState.PLACING:
+        pos = getPos()
+        if Game.exist(pos, availableBoard):
+            Game.addCow(player, Game.findCow(pos, availableBoard), allBoardMills)
+            return 
+        else:
+            print("cow : ", cow.Pos, "Pos : ", (row, col))
+            print("You failed. Try again ")
+            getPlayerMove(player, availableBoard, allBoardMills)
+        if checkIfmill(player, findCow(pos, availableBoard), allBoardMills):
+                return None
+
+    else:
+        print("Where do you want to move the cow from?: ")
+        fromPos = getPos()
+        if not exist(fromPos, player.Cows):
+            print("You have no cow there. Try again") 
+            getPlayerMove(player, availableBoard, allBoardMills)
+        else : 
+            print("Where do you want to move the cow to?: ")
+            toPos = getPos()
+            if Game.exist(toPos, availableBoard):
+                player.Cows = Game.filterOut(player.Cows, fromPos)
+                Game.addCow(player, Game.findCow(toPos, availableBoard))
+            else:
+                print("You can not move your cow there")
+                getPlayerMove(player,availableBoard, allBoardMills)
+
 main()
+
