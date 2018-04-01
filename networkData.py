a908c429e0b64ee613b3c9f67598fa8757ddd9bd
Martin <shukosimango@yahoo.com>
2018-04-01 21:18:28 +0000

    add network_data class for storing data that is sent across sockets

---
+++ networkData.py
@@ -0,0 +1,27 @@
+from enum import Enum
+from __main__ import *
+
+
+class InstructionFromClient( Enum):
+    PLAYER_MOVE = 0
+    KILL_COW=1
+
+
+class InstructionFromServer(Enum): 
+    DO_NOTHING = 0
+    MOVE_PIECE = 1
+    REMOVE_PIECE = 2
+    GAME_START = 3
+
+    
+class Network_data:
+
+    def __init__(self):
+        self.CLIENT_INSTRUCTION=0
+        self.SERVER_INSTRUCTION=0
+        self.latest_player_id=0
+        self.current_player_id=0
+        self.fromLet=''
+        self.fromNum=0
+        self.toLet=''
+        self.toNum=0
