ac69d9f49f9d4d5af8a96efd23523ebea936e45e
Martin <shukosimango@yahoo.com>
2018-04-02 10:00:29 +0000

    add startGame and runGame functions to set up and the run the game on the server

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 
 import socket
-import networkData
-
-
+from networkData import *
+from moraba import *
+from data import *
+from board import printOut, cowSay, colors
 
 
 SERVER_SOCKET=socket.socket();
-NETWORK_DATA= networkData.Network_data()
+NETWORK_DATA= Network_data()
 NUM_PLAYERS=0
 PLAYER_SOCK_ID=[]
 
@@ -14,12 +15,31 @@ def create_server_socket():
     global SERVER_SOCKET
     HOST=socket.gethostbyname(socket.getfqdn())
     print("Host: {0}\n".format(HOST))
-    PORT=socket.htons(networkData.SERVER_PORT)
+    PORT=socket.htons(SERVER_PORT)
     ADDR=(HOST,PORT)
     SERVER_SOCKET.bind(ADDR)
 
+def runGameServer(game):
+    game.nextTurn()
+    checkStateChange(game.CurrentPlayer)
+    #send_clients_game(game)
+    printOut(game);
+
+
+def startGame():
+    player1 = Player("player1",'X',12,ThePlayerState.PLACING,[],colors.GREEN)
+    player2 = Player("player2",'O',12,ThePlayerState.PLACING,[],colors.RED)
+    board= Board()
+    startboard=board.startBoard()
+    allBoardMills= board.allBoardMills()
+    game= Game(player1,player2,startboard,1,allBoardMills)
+    runGameServer(game)
+
+
+
 def main():
-    global SERVER_SOCKET,NUM_PLAYERS
+    global NETWORK_DATA,SERVER_SOCKET,NUM_PLAYERS
+
     create_server_socket()
     SERVER_SOCKET.listen(2)
     print("Server Started\n")
@@ -34,6 +54,16 @@ def main():
         client_sock.send(str(NUM_PLAYERS).encode())
         NUM_PLAYERS+=1
 
+    print("Game can now start!\n")
+    
+    #tell the players that the game can start
+
+    NETWORK_DATA.SERVER_INSTRUCTION= NETWORK_DATA.getServerIntructionValue(InstructionFromServer.GAME_START)
+    
+
+    PLAYER_SOCK_ID[1].send(str(NETWORK_DATA.SERVER_INSTRUCTION).encode())
+
+    startGame()
     PLAYER_SOCK_ID[0].close()
     PLAYER_SOCK_ID[1].close()
     SERVER_SOCKET.close()
