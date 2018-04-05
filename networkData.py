0ad63e4ef55bb5081b3dc794a7867a7089687e1c
Martin <shukosimango@yahoo.com>
2018-04-05 15:59:18 +0200

    change enum classes to regular classes and remove 2 functions

--- networkData.py
+++ networkData.py
@@ -4,12 +4,12 @@ from __main__ import *
 SERVER_PORT=5151
 BUFFER_SIZE= 1024
 
-class InstructionFromClient():
+class InstructionFromClient:
     PLAYER_MOVE = 0
     KILL_COW=1
     NO_KILL_COW=2
 
-class InstructionFromServer(): 
+class InstructionFromServer: 
     DO_NOTHING = 0
     MOVE_PIECE = 1
     REMOVE_PIECE = 2
@@ -26,22 +26,4 @@ class Network_data:
         self.fromPos=('',0)
         self.toPos=('',0)
 
-    def getServerInstructionValue(self,SERVER_INSTRUCTION):
-        if(SERVER_INSTRUCTION==InstructionFromServer.DO_NOTHING):
-            return 0
-        elif (SERVER_INSTRUCTION==InstructionFromServer.MOVE_PIECE):            
-            return 1
-        elif (SERVER_INSTRUCTION==InstructionFromServer.REMOVE_PIECE):            
-            return 2
-        elif (SERVER_INSTRUCTION==InstructionFromServer.GAME_START):            
-            return 3
-    def getClientInstructionValue(self,CLIENT_INSTRUCTION):
-        if(CLIENT_INSTRUCTION==InstructionFromClient.PLAYER_MOVE):           
-            return 0
-        elif(CLIENT_INSTRUCTION==InstructionFromClient.KILL_COW):           
-            return 1
-        elif(CLIENT_INSTRUCTION==InstructionFromClient.NO_KILL_COW):   
-            return 2 
-
-
-
+  
