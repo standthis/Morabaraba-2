86bb84e6af3806d2faa74d1be2e1727d19831672
louise-poole <louisecarmenpoole@gmail.com>
2018-04-09 15:11:06 +0200

    neaten code

--- networkData.py
+++ networkData.py
@@ -1,10 +1,10 @@
-SERVER_PORT=5151
-BUFFER_SIZE= 1024
+SERVER_PORT = 5151
+BUFFER_SIZE = 1024
 
 class InstructionFromClient:
     PLAYER_MOVE = 0
-    KILL_COW=1
-    NO_KILL_COW=2
+    KILL_COW = 1
+    NO_KILL_COW = 2
 
 class InstructionFromServer: 
     DO_NOTHING = 0
@@ -12,15 +12,12 @@ class InstructionFromServer:
     REMOVE_PIECE = 2
     GAME_START = 3
 
-    
 class Network_data:
 
     def __init__(self):
-        self.CLIENT_INSTRUCTION=0
-        self.SERVER_INSTRUCTION=0
-        self.latest_player_id=0
-        self.current_player_id=0
-        self.fromPos=('',0)
-        self.toPos=('',0)
-
-  
+        self.CLIENT_INSTRUCTION = 0
+        self.SERVER_INSTRUCTION = 0
+        self.latest_player_id = 0
+        self.current_player_id = 0
+        self.fromPos = ('', 0)
+        self.toPos = ('', 0)
